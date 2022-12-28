b374cdc0415cc7f62ac35176ee10dbce02efc754
Vishal Kundu <kunduvishal35@gmail.com>
2022-12-28 21:39:22 +0530

    Several New functions were added
    1. Automatic movement is created. Pointer will be able to move according to dice value.
    2. Pointer will change position if eaten by snake or climb up a ladder

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ let dicevalue = document.getElementById('dice');
 const generate = document.querySelector('button');
 const reset = document.querySelector('#reset');
 let DV = document.querySelector('#dicevalue');
+const snakes = {
+  17:7,
+  64:60,
+  89:26,
+  95:74,
+  99:78,
+}
+const LADDERS = {
+  4:14,
+  9:31,
+  20:38,
+  28:84,
+  51:67,
+}
 let posr = 3.0;
 let posc = 3.0;
 let row = 1;
@@ -14,8 +28,33 @@ let pos=0;
 let dice;
 let c = 0;
 
+document.getElementById('normal').addEventListener('click', ()=>{
+  makeMove();  
+    document.getElementById('msg').innerHTML=`Co-ords=> Left: ${posr.toFixed(1)}px , Top: ${posc}px`;
+  });
+
 //RESET BUTTON
 reset.addEventListener('click', gameReset)
+//RESET FUNCTION
+function gameReset(){
+  check = 0;
+  pos=0;
+  dice;
+  c = 0;
+  row = 1;
+  coloum = 1;
+  posr = 3.0;
+  posc = 3.0;
+  pointer.style.left = posr + 'px';
+  pointer.style.top = posc + 'px';
+  document.getElementById('msg').innerHTML=`Co-ords=> Left: ${posr.toFixed(1)}px , Top: ${posc}px`;
+  document.querySelector('.CurrentPosition').innerText= `Current Position: ${pos}`;
+  document.querySelector('#dicevalue').innerText=`Dice Value: none`;
+  document.querySelector('.result').innerText='GAME HAS BEEN RESET';
+  setInterval(()=>{document.querySelector('.result').innerText='';},5000);
+}
+
+//---------------------------------//
 
 //DICE ROLL BUTTON
 generate.addEventListener('click',()=>{
@@ -28,59 +67,98 @@ generate.addEventListener('click',()=>{
     pos=0;
     dice;
     c = 0;
-    posr = 3.0;
-    posc = 3.0;
-    pointer.style.left = posr + 'px';
-    pointer.style.top = posc + 'px';
+    //row = 1;
+    //coloum = 1;
+    //posr = 3.0;
+    //posc = 3.0;
+    //pointer.style.left = posr + 'px';
+    //pointer.style.top = posc + 'px';
     document.querySelector('.CurrentPosition').innerText= `Current Position: ${pos}`;
     document.querySelector('#dicevalue').innerText=`Dice Value: none`;
-    document.querySelector('.result').innerText='game over, you won';
-    setInterval(()=>{document.querySelector('.result').innerText='';},2000);
+    document.querySelector('.result').innerText='YOU WON';
+    setInterval(()=>{document.querySelector('.result').innerText='';},5000);
+    //setTimeout(()=>{gameover()},3500); 
   }
 })
-
-//RESET FUNCTION
-function gameReset(){
-  check = 0;
-  pos=0;
-  dice;
-  c = 0;
-  posr = 3.0;
-  posc = 3.0;
-  pointer.style.left = posr + 'px';
-  pointer.style.top = posc + 'px';
-  document.querySelector('.CurrentPosition').innerText= `Current Position: ${pos}`;
-  document.querySelector('#dicevalue').innerText=`Dice Value: none`;
-  document.querySelector('.result').innerText='game has been reset';
-  setInterval(()=>{document.querySelector('.result').innerText='';},2000);
-}
-
-const snakes = {
-  17:7,
-  64:60,
-  89:26,
-  95:74,
-  99:78,
-}
-
-const LADDERS = {
-  4:14,
-  9:31,
-  20:38,
-  28:84,
-  51:67,
-}
-
-function aumove(dice){
-  let count=0;
-  while(count<dice){
-    makeMove();
-    count++;
-     
+//POSITION CALCULATOR FUNCTION
+function positionaDeterminer(diceVal){
+  let count=0; 
+  if(diceVal==6 && check == 0){
+    pos+=1;
+    check=1;
   }
+  else if(check==1){
+    pos+=diceVal;
+    aumove(diceVal);
 
+  }
+  //snakeCheckg(posr , posc);
+  //ladderCheckg(posr , posc);
+  pos = snakeCheck();
+  pos = ladderCheck();   
+  document.querySelector('.CurrentPosition').innerText= `Current Position: ${pos}`;
+  
 }
-
+//AUTOMATIC MOVEMENT FUNCTION
+function aumove(dice){
+  let count=0;  
+  let v = setInterval(()=>{
+    if(count<dice){
+      makeMove();     
+      count++;
+    }else clearInterval(v);
+  },300);
+  setTimeout(()=>{  document.getElementById('msg').innerHTML=`Co-ords=> Left: ${posr.toFixed(1)}px , Top: ${posc}px`;
+    snakeCheckg(posr , posc);
+    ladderCheckg(posr , posc);
+  } , 2000) 
+           
+  /*while(count<dice){
+    makeMove();
+    count++;     
+  }*/
+}
+//SINGLE SPET MOVEMENT FUNCTION
+function makeMove(){
+ 
+  if(row%2!=0){
+    posr += 48.4;    
+    pointer.style.left = posr + 'px';
+    if(posr>438.6){
+      row++;
+      posc+=52.0;
+      pointer.style.top = posc + 'px';
+    }    
+  }
+  if(row%2==0){
+    posr-=48.4; 
+    pointer.style.left = posr + 'px';    
+    if(posr.toFixed(1)<3){
+      row++; 
+      posc+=52.0;
+      posr=3.0;      
+      pointer.style.left = posr + 'px';
+      pointer.style.top = posc + 'px';         
+    }        
+  }
+  if(posc==471){
+    if(posr.toFixed(1)<=3){
+      pointer.style.left = 3 + 'px';
+      pointer.style.top = 471 + 'px';
+      document.querySelector('.result').innerText='game over, you won';
+      setTimeout(()=>{gameover()},1500);
+    }    
+  }   
+  if(posc>=523){
+    if(posr.toFixed(1)>=3){
+      pointer.style.left = 3 + 'px';
+      pointer.style.top = 471 + 'px';
+      document.querySelector('.result').innerText='game over, you won';
+      setTimeout(()=>{gameover()},1500);
+    }    
+  } 
+  
+}
 //CHECK FOR SNAKES
 function snakeCheckg(rowpos , colpos){
   let k = rowpos.toFixed(1);
@@ -88,10 +166,13 @@ function snakeCheckg(rowpos , colpos){
   
   //pos 17 to pos 7
   if(colpos==3+(52*(2-1)) && k == 3+(48.4*(4-1))){
+    
+    
     posr = 3+(48.4*(7-1));
     posc = 3+(52*(1-1));
     pointer.style.left = posr + 'px';
     pointer.style.top = posc + 'px';
+    new Audio('snakehiss.mp3').play();
     row = 1;    
   }
   
@@ -101,6 +182,7 @@ function snakeCheckg(rowpos , colpos){
     posc = 3+(52*(6-1));
     pointer.style.left = posr + 'px';
     pointer.style.top = posc + 'px';
+    new Audio('snakehiss.mp3').play();
     row = 6;    
   }
   
@@ -110,6 +192,7 @@ function snakeCheckg(rowpos , colpos){
     posc = 3+(52*(3-1));
     pointer.style.left = posr + 'px';
     pointer.style.top = posc + 'px';
+    new Audio('snakehiss.mp3').play();
     row = 3;    
   }
   
@@ -119,6 +202,7 @@ function snakeCheckg(rowpos , colpos){
     posc = 3+(52*(8-1));
     pointer.style.left = posr + 'px';
     pointer.style.top = posc + 'px';
+    new Audio('snakehiss.mp3').play();
     row = 8;    
   }
   
@@ -128,132 +212,102 @@ function snakeCheckg(rowpos , colpos){
     posc = 3+(52*(8-1));
     pointer.style.left = posr + 'px';
     pointer.style.top = posc + 'px';
+    new Audio('snakehiss.mp3').play();
     row = 8;    
   }  
 }
-
 //CHECK FOR LADDERS
 function ladderCheckg(rowpos , colpos){
   let k = rowpos.toFixed(1);
-  //loca.innerHTML = `${k} , ${posc}`;
-  
   //pos 4 to pos 14
   if(colpos==3+(52*(1-1)) && k == 3+(48.4*(4-1))){
     posr = 3+(48.4*(7-1));
     posc = 3+(52*(2-1));
     pointer.style.left = posr + 'px';
-    pointer.style.top = posc + 'px';
-    row = 2;    
-  }
-  
+    pointer.style.top = posc + 'px'; 
+    new Audio('y2mate.com - Climbing Fast  Sound Effect.mp3').play();    
+    row = 2;   
+  }  
   //pos 9 to pos 31
   if(colpos==3+(52*(1-1)) && k == 3+(48.4*(9-1))){
     posr = 3+(48.4*(10-1));
     posc = 3+(52*(4-1));
     pointer.style.left = posr + 'px';
     pointer.style.top = posc + 'px';
+    new Audio('y2mate.com - Climbing Fast  Sound Effect.mp3').play();    
     row = 4;    
-  }
-  
+  }  
   //pos 20 to pos 38
   if(colpos==3+(52*(2-1)) && k == 3+(48.4*(1-1))){
     posr = 3+(48.4*(3-1));
     posc = 3+(52*(4-1));
     pointer.style.left = posr + 'px';
     pointer.style.top = posc + 'px';
+    new Audio('y2mate.com - Climbing Fast  Sound Effect.mp3').play();    
     row = 4;    
-  }
-  
+  } 
   //pos 28 to pos 84
   if(colpos==3+(52*(3-1)) && k == 3+(48.4*(8-1))){
     posr = 3+(48.4*(4-1));
     posc = 3+(52*(9-1));
     pointer.style.left = posr + 'px';
     pointer.style.top = posc + 'px';
+    new Audio('y2mate.com - Climbing Fast  Sound Effect.mp3').play();    
     row = 9;    
-  }
-  
+  }  
   //pos 51 to pos 67
   if(colpos==3+(52*(6-1)) && k == 3+(48.4*(10-1))){
     posr = 3+(48.4*(7-1));
     posc = 3+(52*(7-1));
     pointer.style.left = posr + 'px';
     pointer.style.top = posc + 'px';
+    new Audio('y2mate.com - Climbing Fast  Sound Effect.mp3').play();    
     row = 7;    
   }  
 }
-
-function makeMove(){
-  if(row%2!=0){
-    posr += 48.4;    
-    pointer.style.left = posr + 'px';
-    
-    if(posr>438.6){
-      row++;
-      posc+=52.0;
-      pointer.style.top = posc + 'px';
-    }  
-    
-  }
-  if(row%2==0){
-    posr-=48.4;    
-    pointer.style.left = posr + 'px';    
-    if(posr<2){
-      row++; 
-      posc+=52.0;
-      posr=3.0;      
-      pointer.style.left = posr + 'px';
-      pointer.style.top = posc + 'px';         
-    }      
-  }
-}
-
-//DICE ROLL FUNCTION
-function rollDice(){
-  dice=Math.floor(Math.random()*6)+1;  
-  document.getElementById('dicevalue').innerText=`Dice Value: ${dice}`;
-}
-
 //SNAKE CHECKER FUNCTION
 function snakeCheck(){
   for(let i in snakes){
     if(pos==i){
       pos=snakes[i];
-      new Audio('snakehiss.mp3').play();
+      //new Audio('snakehiss.mp3').play();
       document.querySelector('.result').innerText=`You got swalloed by a snake at position ${i}`;     
     }
     else pos = pos;
   }
   return pos;
 }
-
 //LADDER CHECKER FUNCTION
 function ladderCheck(){
   for(let i in LADDERS){
     if(pos==i){
       pos=LADDERS[i];
-      new Audio('y2mate.com - Climbing Fast  Sound Effect.mp3').play();
+      //new Audio('y2mate.com - Climbing Fast  Sound Effect.mp3').play();
       document.querySelector('.result').innerText=`You climbed up the ladder from position ${i} to ${pos}`;     
     }
     else pos = pos;
   }
   return pos
 }
-
-//POSITION CALCULATOR FUNCTION
-function positionaDeterminer(diceVal){
-    
-  if(diceVal==6 && check == 0){
-    pos+=1;
-    check=1;
-  }
-  else if(check==1){
-    pos+=diceVal;
-    aumove(diceVal);
-  }
-  snakeCheckg(posr , posc);
-  ladderCheckg(posr , posc);
-  pos = snakeCheck();
-  pos = ladderCheck();   
+//DICE ROLL FUNCTION
+function rollDice(){
+  dice=Math.floor(Math.random()*6)+1;  
+  document.getElementById('dicevalue').innerText=`Dice Value: ${dice}`;
+}
+//GAME OVER FUNCTION
+function gameover(){
+  check = 0;
+  pos=0;
+  dice;
+  c = 0;
+  posr = 3.0;
+  posc = 3.0;
+  row = 1;
+  coloum = 1;
+  pointer.style.left = posr + 'px';
+  pointer.style.top = posc + 'px';
   document.querySelector('.CurrentPosition').innerText= `Current Position: ${pos}`;
+  document.querySelector('#dicevalue').innerText=`Dice Value: none`;
+  setInterval(()=>{document.querySelector('.result').innerText='';},5000);
+  document.getElementById('msg').innerHTML=`Co-ords=> Left: ${posr.toFixed(1)}px , Top: ${posc}px`;
 }
